0e00694f03d19fac9c83ed18a29a468afd8fc2a1
MNiko97 <nikola.mitrovic@ulb.ac.be>
2020-12-07 17:44:02 +0100

    add namespace Node

--- Simulator/Node.cs
+++ Simulator/Node.cs
@@ -1,16 +1,16 @@
-using System;
+namespace Node{
+    class Node{
+        public int id;
+        public int nodePower;
+        public bool nodeConnected;
 
-class Node{
-    public int id;
-    public int nodePower;
-    public bool nodeConnected;
-
-    public Node(int id){
-        this.id = id;
-        this.nodePower = 0;
-        this.nodeConnected = false;
-    }
-    public int getNodePower(){
-        return nodePower;
-    }
+        public Node(int id){
+            this.id = id;
+            this.nodePower = 0;
+            this.nodeConnected = false;
+        }
+        public int getNodePower(){
+            return nodePower;
+        }
+    }   
 }
