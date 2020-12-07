8d1f32345f05b796048114fda02e15b4679db3f2
MNiko97 <nikola.mitrovic@ulb.ac.be>
2020-12-07 17:46:33 +0100

    update

--- Simulator/Node.cs
+++ Simulator/Node.cs
@@ -3,7 +3,6 @@ namespace Node{
         public int id;
         public int nodePower;
         public bool nodeConnected;
-
         public Node(int id){
             this.id = id;
             this.nodePower = 0;
