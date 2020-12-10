069156564b686d919b1540e287fb2851a84639b1
MNiko97 <nikola.mitrovic@ulb.ac.be>
2020-12-10 13:15:13 +0100

    update

--- Simulator/Node.cs
+++ Simulator/Node.cs
@@ -1,15 +1,24 @@
-namespace Node{
+namespace Network{
     class Node{
         public int id;
         public int nodePower;
-        public bool nodeConnected;
+        public bool nodeState;
         public Node(int id){
             this.id = id;
             this.nodePower = 0;
-            this.nodeConnected = false;
+            this.nodeState = false;
         }
         public int getNodePower(){
             return nodePower;
+        }
+        public bool getNodeState(){
+            return nodeState;
+        }
+        public void updateNode(){
+            // To implement
+        }
+        public override string ToString(){
+            return base.ToString() + " N" + id.ToString();
         }
     }   
 }
