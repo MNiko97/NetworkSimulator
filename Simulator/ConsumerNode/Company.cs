dc39afb62b6ee1200c96a93f957ae9975896e24d
Pedropinto29 <61003408+Pedropinto29@users.noreply.github.com>
2020-12-30 19:23:43 +0100

    Update Company.cs

--- Simulator/ConsumerNode/Company.cs
+++ Simulator/ConsumerNode/Company.cs
@@ -1,20 +1,12 @@
 using System;
-
 namespace Network{
-
     class Company : ConsumerNode
     {
-        public Company(float energyRequire) : base(energyRequire)
-        {
-        }
-
+        public Company(float energyRequire) : base(energyRequire){}
         public override void setPrice()
         {
             energyPrice = 10*energyRequire;
             Console.WriteLine("Company paid "+energyPrice+" euros for "+energyRequire+" KW");
         }
-        // public override string ToString(){
-        //     return "Company N" + id.ToString();
-        // }
     }
 }
