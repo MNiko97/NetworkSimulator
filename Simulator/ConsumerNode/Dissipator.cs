4f42f69103553237756c50fba659244a23bd2740
Pedropinto29 <61003408+Pedropinto29@users.noreply.github.com>
2020-12-30 19:24:23 +0100

    Update Dissipator.cs

--- Simulator/ConsumerNode/Dissipator.cs
+++ Simulator/ConsumerNode/Dissipator.cs
@@ -1,20 +1,15 @@
 using System;
-
 namespace Network{
-
     class Dissipator : ConsumerNode
     {
         public Dissipator(float energyRequire):base(energyRequire)
         {
             this.isPrioritized = false;
         }
-
-        public override void setPrice(){
+        public override void setPrice()
+        {
             energyPrice = 0;
             Console.WriteLine(energyRequire+" KW were lost because of overproduction");
-        }    
-        // public override string ToString(){
-        //     return "Disspator N" + id.ToString();
-        // }
+        }
     }
 }
