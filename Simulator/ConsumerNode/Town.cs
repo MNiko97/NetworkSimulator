bd2b96a5d886d0a816fd1e6182bffbc96e7b00e9
Pedropinto29 <61003408+Pedropinto29@users.noreply.github.com>
2020-12-30 19:25:50 +0100

    Update Town.cs

--- Simulator/ConsumerNode/Town.cs
+++ Simulator/ConsumerNode/Town.cs
@@ -1,21 +1,12 @@
 using System;
-
 namespace Network{
-
     class Town : ConsumerNode
     {
-        public Town(float energyRequire) : base( energyRequire)
-        {
-
-        }
-
-        public override void setPrice(){
-            
+        public Town(float energyRequire) : base( energyRequire){}
+        public override void setPrice()
+        {    
             energyPrice = 10*energyRequire;
             Console.WriteLine("Town paid "+energyPrice+" euros for "+energyRequire+" KW");
         }
-        // public override string ToString(){
-        //     return "Town N" + id.ToString();
-        // }
     }
 }
